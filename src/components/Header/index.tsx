243617275756275dd956a27f1cba5bc9732d0d30
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-24 17:15:50 -0300

    Criado Component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={`widthBig ${styles.container}`}>
+      <div>
+        <Link href="/">
+          <a>
+            <Image src="/logo.svg" height={26} width={239} alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
