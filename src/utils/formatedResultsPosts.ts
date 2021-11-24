6e5825fada9f7ce7f856588fbb92fa2def8fa866
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-24 17:17:35 -0300

    Criado funcao para formatar os posts da pagina home

---
+++ src/utils/formatedResultsPosts.ts
@@ -0,0 +1,23 @@
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+export function formateResultsPosts(posts: Post[]): Post[] {
+  return posts.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+}
