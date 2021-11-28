3d8b8c4e2513fab3116e67e45d04eaaf37f0b555
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-28 09:16:44 -0300

    Adicionado script do prismic no head das paginas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Document, { Head, Main, Html, NextScript } from 'next/document';
+import { repoName } from '../utils/prismicConfiguration';
 
 export default class MyDocument extends Document {
   render() {
@@ -11,6 +12,12 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
+          />
+
+          <script
+            async
+            defer
+            src={`https://static.cdn.prismic.io/prismic.js?new=true&repo${repoName}`}
           />
         </Head>
 
